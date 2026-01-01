be44dd221b8aa4cc56a5c22ae337ea2842f16818
jonayethossen <jonayethossen8921@gmail.com>
2026-01-01 19:43:44 +0600

    _

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import Navbar from "./Components/Navbar";
 import Slider from "./Hero_Section/Slider";
 import FarmimgCropSection from "./Components/FarmingCropSection";
 import PestControlSection from "./Components/PestControlSection";
+<<<<<<< HEAD
 import ContactFormSection from "./Components/Contract";
 import FooterSection from "./Components/Footer";
 const RootLayout = () => {
+=======
+const App = () => {
+>>>>>>> parent of d5f791d (Footer Done)
   return (
     <>
       <Navbar />
       <Slider />
       <FarmimgCropSection />
       <PestControlSection />
-      <ContactFormSection />
-      <FooterSection />
     </>
   );
 };
